30914cbaada7c4e23ce17c9919f898ded006a07f
Sejla Jahic <sejla_jahic@hotmail.com>
2019-06-06 13:33:40 +0200

    added btnNapraviTiket

--- Liga.aspx.cs
+++ Liga.aspx.cs
@@ -5,17 +5,53 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Security;
+using System.Data;
 
 public partial class Liga : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         Label1.Text = (string)Session["New"];
+        if (!this.IsPostBack)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Name"), new DataColumn("Country") });
+            dt.Rows.Add("ManUTD VS RMA 1", "3.45");
+            dt.Rows.Add("ManUTD VS RMA 2", "1.55");
+            dt.Rows.Add("ManUTD VS RMA X", "4.0");
+            dt.Rows.Add("Barcelona VS Tottenham 1", "2.0");
+            dt.Rows.Add("Barcelona VS Tottenham 2", "4.3");
+            dt.Rows.Add("Barcelona VS Tottenham X", "6.2");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
     }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
         Session["New"] = null;
         Response.Redirect("Start.aspx");
+    }
+
+
+    protected void GetSelectedRecords(object sender, EventArgs e)
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Name"), new DataColumn("Country") });
+        foreach (GridViewRow row in GridView1.Rows)
+        {
+            if (row.RowType == DataControlRowType.DataRow)
+            {
+                CheckBox chkRow = (row.Cells[0].FindControl("chkRow") as CheckBox);
+                if (chkRow.Checked)
+                {
+                    string name = row.Cells[1].Text;
+                    string country = (row.Cells[2].FindControl("lblCountry") as Label).Text;
+                    dt.Rows.Add(name, country);
+                }
+            }
+        }
+        gvSelected.DataSource = dt;
+        gvSelected.DataBind();
     }
 }
--- supertip.aspx
+++ supertip.aspx
@@ -1,10 +1,48 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="supertip.aspx.cs" Inherits="supertip" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
    <div style="text-align:right">
         <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
         <asp:Button ID="Button1" runat="server" Text="Logout" OnClick="Button1_Click" /></div>
+
+  
+
+    
+    <asp:GridView ID="GridView1" runat="server" HeaderStyle-BackColor="#3AC0F2" HeaderStyle-ForeColor="White"
+    AutoGenerateColumns="False">
+    <Columns>
+        <asp:TemplateField>
+            <ItemTemplate>
+                <asp:CheckBox ID="chkRow" runat="server" />
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:BoundField DataField="Name" HeaderText="UtakmicaIKladjenje" ItemStyle-Width="150" />
+        <asp:TemplateField HeaderText="Kvota" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:Label ID="lblCountry" runat="server" Text='<%# Eval("Country") %>'></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+<br />
+<asp:Button ID="btnGetSelected" runat="server" Text="NapraviTiket" OnClick="GetSelectedRecords" />
+<hr />
+<u>Tiket</u>
+<br />
+<asp:GridView ID="gvSelected" runat="server" HeaderStyle-BackColor="#3AC0F2" HeaderStyle-ForeColor="White"
+    AutoGenerateColumns="false">
+    <Columns>
+        <asp:BoundField DataField="Name" HeaderText="UtakmicaIKladjenje" ItemStyle-Width="150" />
+        <asp:BoundField DataField="Country" HeaderText="Kvota" ItemStyle-Width="150" />
+    </Columns>
+</asp:GridView>
+
+
+
+
+
 </asp:Content>
 
